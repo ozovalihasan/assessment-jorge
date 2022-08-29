aaf0fc50bad052dc52d0298da36197d832692e47
ozovalihasan <ozovalihasan@gmail.com>
2022-08-29 22:27:14 +0300

    Add Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="bg-red-500">
+      
     </div>
   )
 }
